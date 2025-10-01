94e9a09efe62b80674c806459f477eb9d526382e
EXVYS <versedixz@gmail.com>
2025-10-01 08:43:37 -0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/EXVYS/LuaLibrary/refs/heads/main/luahub.lua"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/661f2224bf854779b07529237cca397f"))()
