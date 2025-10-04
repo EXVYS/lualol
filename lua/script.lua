a2e1f6fe7e658ae8564bcc95be91df9f4caca540
EXVYS <versedixz@gmail.com>
2025-10-04 13:38:51 -0700

    Update script.lua

--- lua/script.lua
+++ lua/script.lua
@@ -1,1 +1,14 @@
+Content-Type: text/html
+Location: https://lualol.com/lua
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="refresh" content="0; url=https://lualol.com/lua">
+</head>
+<body>
+    Redirecting to Lua Hub...
+</body>
+</html>
+
 loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/cd04c19d772a407d86b9a83495ab59ec"))()
